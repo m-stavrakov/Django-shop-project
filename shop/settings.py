89bf669e8db1aaaa0e0cf385c0d8b630546d573c
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-06-08 10:53:55 +0100

    superuser render

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOST', '')]
 
